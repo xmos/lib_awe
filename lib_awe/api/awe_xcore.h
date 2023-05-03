4ab899e2c09ffd3e1bc679f456e4984c103730fd
Henk Muller <henk@xmos.com>
2023-05-03 20:16:37 +0100

    Fixed new C code for AWE tuning

--- lib_awe/api/awe_xcore.h
+++ lib_awe/api/awe_xcore.h
@@ -4,9 +4,20 @@
 #include <stdint.h>
 #include <xcore/channel.h>
 
-/** Thread that implements the AWE DSP functionality. This function does not
- * return and forks into multiple threads: one thread for control, one thread
- * data transport, and AWE_DSP_THREAD_NUM threads for DSP.
+/** Function that spawns all the child threads. This function does not return.
+ * It always spawns at least one tuning thread, one data-transport
+ * thread, and one DSP thread. Up to four additional DSP threads are spawned
+ * for a maximum of seven threads.
+ *
+ * The data-transport thread has a channel to each DSP thread in order
+ * to start them when data is available. A single channel-end is used to send
+ * audio-samples to the data-transport thread and receive samples back.
+ *
+ * The tuning thread occupies another channel-end, it is used for sending
+ * tuning packets to the AWE library. Part packets are sent over this channel
+ * and assembled in the tuning thread; when a complete packet has arrived
+ * the tuning packet is handled by AWE and a reply is sent back over the same
+ * channel-end.
  *
  * You must create a awe_conf.h file to configure the AWE instantiation.
  * This defines the number of channels in the audio stack (AUDIO_INPUT_CHANNELS,
@@ -14,19 +25,29 @@
  * (AWE_INPUT_CHANNELS, AWE_OUTPUT_CHANNELS), and the number of threads that
  * should be used for DSP (AWE_DSP_THREAD_NUM).
  *
- * \param c_control   The channel end over which control packets arrive
+ * \param c_tuning    The channel end over which tuning packets arrive. Over
+ *                    this channel you will send a sequence [N, N words, CT_END],
+ *                    and then receive an [CT_END] empty message as ack. You do
+ *                    this until all data has been sent through.
+ *                    A [CT_END] empty message will be sent back over c_tuning,
+ *                    which you must acknowledge with a word M for the maximum
+ *                    packet size you are willing to accept, whereupon you will
+ *                    be sent [M words, CT_END] that you must acknowledge with a
+ *                    [CT_END]. This is repeated until the reply has been sent
+ *                    back.
  *
  * \param c_data      The channelend over which audio is communicated. Either
  *                    use ``awe_offload_data_to_dsp_engine`` or output
- *                    AUDIO_INPUT_CHANNELS ints over the channel followed by
- *                    CT_END, then input AUDIO_OUTPUT_CHANNELS ints over the 
- *                    channelend and then input a CT_END.
+ *                    [ AUDIO_INPUT_CHANNELS words, CT_END] over c_data, then
+ *                    input [ AUDIO_OUTPUT_CHANNELS, CT_END ] from c_data.
  */
 extern void awe_xcore_main(chanend_t c_control, chanend_t c_data);
 
 /** Convenience function that pushes an audio frame stored in an array to the
  * AWE stack. This function may be avoided and instead data can be pushed
  * directly onto the channelend.
+ *
+ * NOTE: THIS DESCRIPTION IS WRONG AT PRESENT
  *
  * \param c_data      Channelend for audio communication
  *
--- lib_awe/src/awe_tuning.c
+++ lib_awe/src/awe_tuning.c
@@ -1,12 +1,13 @@
-#if 0
+#if 1
 #include <xcore/channel.h>
+#include <xcore/select.h>
 #include "awe_xcore_internal.h"
 
 // TODO: change USB side to fit this
 // TODO: cleanup stacksize
 
 #pragma stackfunction 2048
-void awe_tuning_thread(chanend_t c_control) {
+void awe2_tuning_thread(chanend_t c_control) {
     while(1) {
         int input_message_word_count = 0;
         int full_packet_in_words = 0;
@@ -22,6 +23,7 @@ void awe_tuning_thread(chanend_t c_control) {
                 }
                 AWE_Packet_Buffer[input_message_word_count++] = x;
             }
+            chanend_out_end_token(c_control);
             chanend_check_end_token(c_control);
         } while(input_message_word_count < full_packet_in_words);
         awe_packetProcess((AWEInstance *)&g_AWEInstance);
@@ -30,85 +32,85 @@ void awe_tuning_thread(chanend_t c_control) {
             full_packet_in_words = AWE_HID_PACKET_BUFFER_SIZE;
         }
         int output_message_word_count = 0;
+        chanend_out_end_token(c_control);
+        int max_words = chanend_in_word(c_control);
         do {
-            chanend_out_end_token(c_control);
-            int max_words = chanend_in_word(c_control);
             chanend_check_end_token(c_control);
-            int words = full_packet_in_words > max_words ? max_words : full_packet_in_words;
+            int num_words = (full_packet_in_words > max_words ?
+                             max_words :
+                             full_packet_in_words);
             chanend_out_word(c_control, num_words);
             for(int i = 0; i < num_words; i++) {
-                x = AWE_Packet_Buffer_Reply[output_message_word_count++];
+                int x = AWE_Packet_Buffer_Reply[output_message_word_count++];
                 chanend_out_word(c_control, x);
             }
             chanend_out_end_token(c_control);
+            chanend_check_end_token(c_control);
         } while(output_message_word_count < full_packet_in_words);
     }
 }
 
-#if defined(AWE_USB_HID)
-#include "xud_device.h"                 /* XMOS USB Device Layer defines and functions */
+#if defined(AWE_TUNING_OVER_USB_HID)
+#include "xud.h"
+#include "xud_device.h"
 #include "xua_endpoint0.h"
 
-void awe_usb_hid(chanend c_hid_to_host, chanend c_hid_from_host, chanend c_hid_control) {
+void awe_usb_hid(chanend_t c_hid_to_host, chanend_t c_hid_from_host, chanend_t c_tuning) {
     XUD_ep ep_hid_from_host = XUD_InitEp(c_hid_from_host);
-    XUD_ep ep_hid_to_host = XUD_InitEp(c_hid_to_host);
+    XUD_ep ep_hid_to_host   = XUD_InitEp(c_hid_to_host);
 
-
+    // TODO: these can be combined into one.
     unsigned int g_hid_from_host_buffer[16];
     unsigned int g_hid_to_host_buffer[16];
-    xc_ptr hid_from_host_buffer = array_to_xc_ptr(g_hid_from_host_buffer);
     
-
-    
-    /* Mark OUT endpoints ready to receive data from host */
-    XUD_SetReady_OutPtr(ep_hid_from_host, hid_from_host_buffer);
+    /* Mark the OUT endpoint to be ready to receive data from host */
+    XUD_SetReady_Out(ep_hid_from_host, (unsigned char*)g_hid_from_host_buffer);
 
     int ready_for_next_out_packet = 1;
-    while(1)
-    {
-        XUD_Result_t result;
-        unsigned length;
-        unsigned char ct;
+    XUD_Result_t result;
+    unsigned length;
         
-        /* Wait for response from XUD and service relevant EP */
-        select
-        {
-            case ready_for_next_out_packet => inct_byref(c_hid_control, ct):
-                outuint(c_hid_control, 13);
-                outct(c_hid_control, 1);
-                int num_words = inuint(c_hid_control);
-                g_hid_to_host_buffer[0] = ((num_words << 2) << 24) | 0x00000001;
-                for(int i = 0; i < num_words; i++) {
-                    g_hid_to_host_buffer[i+1] = inuint(c_hid_control);
-                }
-                chkct(c_hid_control, 1);
-                XUD_SetReady_In(ep_hid_to_host, (g_hid_to_host_buffer, unsigned char[]), 56);
-                ready_for_next_out_packet = 0;
-                break;
-                
-            /* HID OUT from host */
-            case XUD_GetData_Select(c_hid_from_host, ep_hid_from_host, length, result):
-                if (result == XUD_RES_OKAY) {
-                    uint32_t header = g_hid_from_host_buffer[0];
-                    uint32_t bytecount = header >> 24;
-                    uint32_t num_words = bytecount >> 2;
-                    outuint(c_hid_control, num_words);
-                    for(int i = 0; i < num_words; i++) {
-                        outuint(c_hid_control, g_hid_from_host_buffer[i+1]);
-                    }
-                    outct(c_hid_control, 1);
-                }
-                XUD_SetReady_OutPtr(ep_hid_from_host, hid_from_host_buffer);
-                break;
-
-            /* HID IN to host */
-            case XUD_SetData_Select(c_hid_to_host, ep_hid_to_host, result):
-                /* The buffer has been sent to the host, so we can ack the hid thread */
-                ready_for_next_out_packet = 1;
-                break;
-
-
+    /* Wait for response from XUD and service relevant EP */
+    SELECT_RES(
+        CASE_GUARD_THEN(c_tuning, ready_for_next_out_packet, input_from_awe),
+        CASE_THEN(c_hid_from_host, xud_hid_output),
+        CASE_THEN(c_hid_to_host, xud_hid_input)
+        )
+    {
+    input_from_awe: /* Data frmo the AWE stack */
+        chanend_check_end_token(c_tuning);
+        chanend_out_word(c_tuning, 13);
+        chanend_out_end_token(c_tuning);
+        int num_words = chanend_in_word(c_tuning);
+        g_hid_to_host_buffer[0] = ((num_words << 2) << 24) | 0x00000001;
+        for(int i = 0; i < num_words; i++) {
+            g_hid_to_host_buffer[i+1] = chanend_in_word(c_tuning);
         }
+        chanend_out_end_token(c_tuning);
+        chanend_check_end_token(c_tuning);
+        XUD_SetReady_In(ep_hid_to_host, (unsigned char *)g_hid_to_host_buffer, 56);
+        ready_for_next_out_packet = 0;
+        continue;
+    xud_hid_output: /* HID OUT from host */
+        XUD_GetData_Select(c_hid_from_host, ep_hid_from_host, &length, &result);
+        if (result == XUD_RES_OKAY) {
+            uint32_t header = g_hid_from_host_buffer[0];
+            uint32_t bytecount = header >> 24;
+            uint32_t num_words = bytecount >> 2;
+            chanend_out_word(c_tuning, num_words);
+            for(int i = 0; i < num_words; i++) {
+                chanend_out_word(c_tuning, g_hid_from_host_buffer[i+1]);
+            }
+            chanend_out_end_token(c_tuning);
+            chanend_check_end_token(c_tuning);
+        }
+        XUD_SetReady_Out(ep_hid_from_host, (unsigned char*)g_hid_from_host_buffer);
+        continue;
+    xud_hid_input:  /* HID IN to host */
+        XUD_SetData_Select(c_hid_to_host, ep_hid_to_host, &result);
+        ready_for_next_out_packet = 1;
+        continue;
+
     }
 }
 
