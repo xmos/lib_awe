640eabcd5f147b72e56c4e91c71860afd60733f1
Ed <ed@xmos.com>
2024-05-16 14:55:30 +0100

    FIrst working XCCM app

---
+++ app_zoom_demo/src/awe_conf.h
@@ -0,0 +1,14 @@
+#ifndef _awe_conf_h_
+#define _awe_conf_h_
+
+#define AWE_DSP_THREAD_NUM           5
+#define AWE_INPUT_CHANNELS           2
+#define AWE_OUTPUT_CHANNELS          2
+#define AUDIO_INPUT_CHANNELS        16
+#define AUDIO_OUTPUT_CHANNELS       16
+
+#define AWE_HEAP_SIZE         (37*1024)
+
+#define AWE_TUNING_OVER_USB_HID      1
+
+#endif
---
+++ lib_awe/lib_build_info_sources.cmake
@@ -0,0 +1,50 @@
+# Function to replace a substring in each entry of a list
+function(replace_substring_in_list list result substring_to_replace replacement_substring)
+    set(temp_list)
+    foreach(item ${${list}})
+        string(REPLACE ${substring_to_replace} "${replacement_substring}" modified_item ${item})
+        list(APPEND temp_list ${modified_item})
+    endforeach()
+    set(${result} ${temp_list} PARENT_SCOPE)
+endfunction()
+
+message(STATUS APP_C_SRCS: ${APP_C_SRCS})
+message(STATUS APP_ROOT_DIR: ${APP_ROOT_DIR})
+
+replace_substring_in_list(APP_C_SRCS APP_C_SRCS ${APP_ROOT_DIR}/ "")
+replace_substring_in_list(APP_ASM_SRCS APP_ASM_SRCS ${APP_ROOT_DIR}/ "")
+
+file(GLOB LIB_AWE_C_SRCS ${CMAKE_CURRENT_LIST_DIR}/src/*.c)
+file(GLOB LIB_AWE_ASM_SRCS ${CMAKE_CURRENT_LIST_DIR}/src/*.S)
+set(LIB_AWE_INCLUDES    ${CMAKE_CURRENT_LIST_DIR}/api
+                        ${CMAKE_CURRENT_LIST_DIR}/src/include_internal)
+
+replace_substring_in_list(LIB_AWE_C_SRCS LIB_AWE_C_SRCS ${CMAKE_CURRENT_LIST_DIR}/ "../lib_awe/")
+replace_substring_in_list(LIB_AWE_ASM_SRCS LIB_AWE_ASM_SRCS ${CMAKE_CURRENT_LIST_DIR}/ "../lib_awe/")
+# replace_substring_in_list(LIB_AWE_INCLUDES LIB_AWE_INCLUDES ${CMAKE_CURRENT_LIST_DIR}/ "../lib_awe/")
+
+list(APPEND APP_C_SRCS ${LIB_AWE_C_SRCS})
+list(APPEND APP_ASM_SRCS ${LIB_AWE_ASM_SRCS})
+list(APPEND APP_INCLUDES ${LIB_AWE_INCLUDES})
+list(APPEND APP_COMPILER_FLAGS -D__awe_conf_h_exists__=1)
+
+message(STATUS APP_C_SRCS: ${APP_C_SRCS})
+message(STATUS APP_ASM_SRCS: ${APP_ASM_SRCS})
+message(STATUS APP_INCLUDES: ${APP_INCLUDES})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
