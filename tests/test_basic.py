85fd655fca94d8ad257df112e5179eb8e4154934
Ed <ed@xmos.com>
2024-06-06 10:53:43 +0100

    Wrapper for test basic so it just sends the command rather than a string

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -16,10 +16,17 @@ xe = "test_basic/bin/test_awe_basic.xe"
 
 
 class awe_cmds:
-    get_target_info = "00020029 00020029"
-    get_moduleclass_count = "0002000d 0002000d"
-    get_core_list = "0002007f 0002007f"
-    get_cpu_usage = "0002002b 0002002b"
+    get_target_info = 0x00020029
+    get_moduleclass_count = 0x0002000d
+    get_core_list = 0x0002007f
+    get_cpu_usage = 0x0002002b
+
+
+def send_command(xe, cmd):
+    crc = 0 ^ cmd
+    cmd_str = f"{hex(cmd)} {hex(crc)}"
+    cmd_str = cmd_str.replace("0x", "")
+    return run_xe(xe, cmd_str)
 
 def run_xe(bin_dir, cmds):
     cmd = f"xsim --max-cycles 1000000 --args {bin_dir} {cmds}"
@@ -37,7 +44,7 @@ def check_expected(dut, expected):
 
 
 def test_target_info():
-    dut = run_xe(xe, awe_cmds.get_target_info)
+    dut = send_command(xe, awe_cmds.get_target_info)
     expected = "000e0000 00000000 473b8000 4b3ebc20 00403020 24020264 08440100 00000107 534f4d58 4253555f 4cbebc20 00000000 7a6b5c4d 07c4f609"
 
     to_fp = lambda a : struct.unpack('f', struct.pack('I', a))[0]
@@ -50,14 +57,14 @@ def test_target_info():
 
 def test_get_moduleclass_count():
     count = 325
-    dut = run_xe(xe, awe_cmds.get_moduleclass_count)
+    dut = send_command(xe, awe_cmds.get_moduleclass_count)
     expected = f"00030000 00000{count:x} 00030{count:x}"
 
     check_expected(dut, expected)
 
 def test_get_core_list():
     cores = 1
-    dut = run_xe(xe, awe_cmds.get_core_list)
+    dut = send_command(xe, awe_cmds.get_core_list)
     expected = f"00040000 0000000{cores} 0000000{cores-1} 0004000{cores}"
 
     check_expected(dut, expected)
