324b591e569ee69188c1f0ad68bd2cf11945ac02
Ed <ed@xmos.com>
2024-06-10 17:54:23 +0100

    WIP test output

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,74 +38,29 @@ def samp_freqs_upto(max):
 
 
 def parse_features(board, config):
-    max_analogue_chans = 8
+    print(board)
 
-    config_re = r"^(?P<uac>[12])(?P<sync_mode>[ADS])(?P<i2s>[MSX])i(?P<chan_i>\d+)o(?P<chan_o>\d+)(?P<midi>[mx])(?P<spdif_i>[sx])(?P<spdif_o>[sx])(?P<adat_i>[ax])(?P<adat_o>[ax])(?P<dsd>[dx])(?P<tdm8>(_tdm8)?)"
-    match = re.search(config_re, config)
-    if not match:
-        pytest.exit(f"Error: Unable to parse features from {config}")
-
-    # Form dictionary of features then convert items to integers or boolean as required
-    features = match.groupdict()
-    for k in ["uac", "chan_i", "chan_o"]:
-        features[k] = int(features[k])
-    for k in ["midi", "spdif_i", "spdif_o", "adat_i", "adat_o", "dsd", "tdm8"]:
-        features[k] = features[k] not in ["", "x"]
-
-    if features["uac"] not in [1, 2]:
-        pytest.exit(f"Error: Invalid UAC in {config}")
-
-    if board == "xk_216_mc":
-        if config.startswith("1"):
-            features["pid"] = 0xF
-        else:
-            features["pid"] = 0xE
-    elif board == "xk_316_mc":
-        if config.startswith("1"):
-            features["pid"] = 0x17
-        else:
-            features["pid"] = 0x16 if "_winbuiltin" not in config else 0x1a
-    elif board == "xk_evk_xu316":
-        if config.startswith("1"):
-            features["pid"] = 0x19
-        else:
-            features["pid"] = 0x18
-
-    # Set the number of analogue channels
-    features["analogue_i"] = min(features["chan_i"], max_analogue_chans)
-    features["analogue_o"] = min(features["chan_o"], max_analogue_chans)
-    if "noi2s" in config:
-        # Force analogue channels to zero
-        features["analogue_i"] = 0
-        features["analogue_o"] = 0
-
-    # Create list of supported sample frequencies
-    if config == "1AMi8o2xxxxxx":
-        features["samp_freqs"] = samp_freqs_upto(44100)
-    elif features["uac"] == 1:
-        if features["chan_i"] and features["chan_o"]:
-            features["samp_freqs"] = samp_freqs_upto(48000)
-        else:
-            features["samp_freqs"] = samp_freqs_upto(96000)
-    elif features["chan_i"] >= 32 or features["chan_o"] >= 32:
-        features["samp_freqs"] = samp_freqs_upto(48000)
-    elif (features["adat_i"] == False and features["chan_i"] >= 16) or (features["adat_o"] == False and features["chan_o"] >= 16):
-        features["samp_freqs"] = samp_freqs_upto(96000)
-    elif (features["adat_i"] == True and features["chan_i"] > 16) or (features["adat_o"] == True and features["chan_i"] > 16): #i18o18 build
-        features["samp_freqs"] = samp_freqs_upto(96000)
-    elif features["tdm8"]:
-        features["samp_freqs"] = samp_freqs_upto(96000)
-    else:
-        features["samp_freqs"] = samp_freqs_upto(192000)
-
+    features = dict()
+    features["uac"] = 2
+    features["chan_i"] = 0
+    features["chan_o"] = 2
+    features["midi"] = None
+    features["spdif_i"] = None
+    features["spdif_o"] = None
+    features["adat_i"] = None
+    features["adat_o"] = None
+    features["dsd"] = None
+    features["tdm8"] = None
+    features["samp_freqs"] = [48000]
     features["i2s_loopback"] = False
+
     return features
 
 
 def pytest_sessionstart(session):
     usb_audio_dir = Path(__file__).parents[1]
-    app_prefix = "app_usb_aud_"
-    exclude_app = ["app_usb_aud_xk_evk_xu316_extrai2s"]
+    app_prefix = "app_"
+    exclude_app = ["usb_audio_awe_small_qf60", "usb_audio_awe_small", "zoom_demo"]
 
     test_level = session.config.getoption("level")
 
@@ -161,7 +116,8 @@ def list_configs():
 
 
 def get_config_features(board, config):
-    return board_configs[f"{board}-{config}"]
+    return board_configs[f"usb_audio_awe-I2S"]
+    # return board_configs[f"{board}-{config}"]
 
 
 # Dictionary indexed by board name, with each entry being the tuple of XTAG IDs for
@@ -178,24 +134,25 @@ def pytest_configure(config):
         dut = config.getini(f"{board}_dut")
         harness = config.getini(f"{board}_harness")
         xtag_dut_harness[board] = (dut, harness)
+        print("***", board, dut, harness)
 
 
-def pytest_collection_modifyitems(config, items):
-    selected = []
-    deselected = []
+# def pytest_collection_modifyitems(config, items):
+#     selected = []
+#     deselected = []
 
-    # Deselect testcases which use hardware that doesn't have an XTAG ID
-    for item in items:
-        m = item.get_closest_marker("uncollect_if")
-        if m:
-            func = m.kwargs["func"]
-            if func(config, **item.callspec.params):
-                deselected.append(item)
-            else:
-                selected.append(item)
+#     # Deselect testcases which use hardware that doesn't have an XTAG ID
+#     for item in items:
+#         m = item.get_closest_marker("uncollect_if")
+#         if m:
+#             func = m.kwargs["func"]
+#             if func(config, **item.callspec.params):
+#                 deselected.append(item)
+#             else:
+#                 selected.append(item)
 
-    config.hook.pytest_deselected(items=deselected)
-    items[:] = selected
+#     config.hook.pytest_deselected(items=deselected)
+#     items[:] = selected
 
 
 # Print a session-level warning if usbdeview is not available on Windows
--- tests/test_audio.py
+++ tests/test_audio.py
@@ -7,7 +7,8 @@ Runs audio through a basic design and checks for glitching
 import pytest
 from pathlib import Path
 import os, sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "../../sw_usb_audio/tests"))
+sw_usb_audio_test_path = "../../sw_usb_audio/tests"
+sys.path.append(os.path.join(os.path.dirname(__file__), sw_usb_audio_test_path))
 from usb_audio_test_utils import (
     check_analyzer_output,
     get_xtag_dut_and_harness,
@@ -17,8 +18,70 @@ from usb_audio_test_utils import (
     XsigOutput,
     xsig_completion_time_s
 )
-# from test_analogue import test_analogue_output
+from conftest import list_configs, get_config_features, board_configs
+from test_analogue import analogue_duration
 
-def test_no_glitches():
-    print("Pass")
+@pytest.mark.parametrize(["board", "config"], list_configs())
+def test_output(pytestconfig, board, config):
+    features_tmp = get_config_features(board, config)
+    features = features_tmp.copy()
+
+    xsig_config = "mc_analogue_output_2ch"
+    xsig_config_path = Path(__file__).parent / sw_usb_audio_test_path / "xsig_configs" / f"{xsig_config}.json"
+
+    board = "xk_316_mc"
+    adapter_dut, adapter_harness = get_xtag_dut_and_harness(pytestconfig, board)
+    # adapter_dut, adapter_harness = get_xtag_dut_and_harness(pytestconfig, board)
+
+    short_test = features["partial"] or board == "xk_316_mc"
+    duration = analogue_duration(pytestconfig.getoption("level"), short_test)
+
+    print("$$$$$", board, config, adapter_dut)
+    print("-----", board_configs)
+
+    fail_str = ""
+
+    adapter_dut = "kjhkjhkjh"
+
+    #    subprocess.run(["xrun", "--adapter-id", self.adapter_id, firmware], timeout=self.timeout)
+    #    def __init__(self, adapter_id, board, config, timeout=30):
+
+    # Override stuff
+    xrun_dut = XrunDut(adapter_dut, board, config)
+    xrun_dut.firmware_override = "../app_usb_audio_awe/bin/UA/app_usb_audio_awe_UA.xe"
+    xrun_dut.config = "2"
+    xrun_dut.adapter_id = "BJ7H71BA"
+
+    with xrun_dut as dut:
+        for fs in features["samp_freqs"]:
+            # Issue 120
+            if (
+                platform.system() == "Windows"
+                and board == "xk_316_mc"
+                and config == "2AMi8o8xxxxxx_winbuiltin"
+                and fs in [44100, 48000]
+            ):
+                continue
+
+            dut.set_stream_format("output", fs, features["chan_o"], 24)
+
+            with (
+                AudioAnalyzerHarness(adapter_harness, xscope="io") as harness,
+                XsigOutput(fs, None, xsig_config_path, dut.dev_name),
+            ):
+                time.sleep(duration)
+                harness.terminate()
+                xscope_lines = harness.get_output()
+
+            with open(xsig_config_path) as file:
+                xsig_json = json.load(file)
+            failures = check_analyzer_output(xscope_lines, xsig_json["out"])
+            if len(failures) > 0:
+                fail_str += f"Failure at sample rate {fs}\n"
+                fail_str += "\n".join(failures) + "\n\n"
+                fail_str += f"xscope stdout at sample rate {fs}\n"
+                fail_str += "\n".join(xscope_lines) + "\n\n"
+
+    if len(fail_str) > 0:
+        pytest.fail(fail_str)
 
