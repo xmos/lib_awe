d9ce8edbca901cfdc4e75f01a7bb2b9c58b9e46c
Ed <ed@xmos.com>
2024-06-14 11:55:21 +0100

    Improve error reporting in awb loader util

--- tests/awe_test_utils.py
+++ tests/awe_test_utils.py
@@ -9,10 +9,75 @@ import numpy as np
 import time
 import re
 import subprocess
+import time
+
+
+
+class awe_cmd_list:
+    """
+    Reads the commands file and turns them into the enum for sending cmds
+    """
+    def __init__(self, file="../lib_awe/src/include_internal/ProxyIDs.h"):
+        self.enum_dict = {}
+
+        with open(file) as ef:
+            file_content = ef.read()
+            enum_pattern = re.compile(r"typedef\s+enum\s*{([^}]*)}\s*(\w+);", re.DOTALL)
+            matches = enum_pattern.search(file_content)
+            if matches:
+                enum_body = matches.group(1)
+                # Remove all comments
+                enum_body = re.sub(r'//.*?\n|/\*.*?\*/', '', enum_body, flags=re.DOTALL)
+                enum_entries = enum_body.split(',')
+                current_value = -1
+                for entry in enum_entries:
+                    entry = entry.strip()
+                    if entry:
+                        if '=' in entry:
+                            name, value = entry.split('=')
+                            name = name.strip()
+                            value = int(value.strip())
+                            current_value = value
+                        else:
+                            name = entry.strip()
+                            current_value += 1
+                        self.enum_dict[name] = current_value
+
+    def lookup(self, cmd):
+        return self.enum_dict[cmd]
+
+    def get_keys(self):
+        return self.enum_dict
+
+class awe_error_codes:
+    """
+    Reads the error file and turns them into a string from the err code
+    """
+    def __init__(self, file="../lib_awe/src/include_internal/Errors.h"):
+        self.file = file
+        self.define_dict = self._parse_defines()
+
+    def _parse_defines(self):
+        define_dict = {}
+        define_pattern = re.compile(r"#define\s+(\w+)\s+\(([-\d]+)\)")
+        
+        with open(self.file, 'r') as ef:
+            file_content = ef.read()
+            for match in define_pattern.findall(file_content):
+                name, value = match
+                define_dict[int(value)] = name
+        
+        return define_dict
+
+    def lookup(self, err_code):
+        return self.define_dict.get(err_code, None)
+
+    def get_keys(self):
+        return self.define_dict
 
 
 # Get the udev rules sorted in linux https://depts.washington.edu/dxscdoc/Help/Guides/HID_permissions.html#:~:text=On%20Linux&text=This%20will%20give%20read%20and,plugging%20and%20replugging%20the%20device.
-class awe_hid_comms:
+class awe_hid_comms(awe_error_codes, awe_cmd_list):
     """
         See: https://dspconcepts.com/sites/default/files/forum/awe-core-integration-guide.pdf
     """
@@ -20,19 +85,24 @@ class awe_hid_comms:
     resp_a = 0x30000
     resp_b = 0x40000
 
-    def __init__(self, VID=0x20b1, PID=0x18):
+    def __init__(self, VID=0x20b1, PID=0x18, debug=False):
         np.set_printoptions(formatter={'int':hex})
         self.awe_hid_len = 56
 
+        awe_error_codes.__init__(self)
+        awe_cmd_list.__init__(self)
+
         # For debug
-        # devices = hid.enumerate()
-        # for device in devices:
-        #     print(device)
+        if debug:
+            devices = hid.enumerate()
+            for device in devices:
+                print(device)
 
         self.dev = hid.device()
         self.dev.open(VID, PID)
 
-        # print(f"Connected to 0x{VID:x} 0x{PID:x}")
+        if debug:
+            print(f"Connected to 0x{VID:x} 0x{PID:x}")
 
 
     def cmd(self, msg):
@@ -148,7 +218,8 @@ class awe_hid_comms:
 
         # Stop audio first to avoid frequent firmware exception when destroying existing design
         # See https://xmosjira.atlassian.net/wiki/spaces/UAAI/pages/4122148883/DSPC+Integration+snag+list
-        self.cmd([0x2001C])
+        self.cmd([0x20000 + awe_cmd_list.lookup(self, 'PFID_StopAudio')])
+        time.sleep(0.001) # 1ms to allow stop audio to complete
 
         awb_idx = 0
         while(awb_idx <= awb_data_len):
@@ -160,71 +231,11 @@ class awe_hid_comms:
             cmd = awb_data[awb_idx : awb_idx + cmd_len]
             err = self.cmd(list(cmd))
             if err:
-                print("Non-zero response: {err}")
+                err_2s_compl = struct.unpack('<i', struct.pack('<I', err))[0]
+                err_str = awe_error_codes.lookup(self, err_2s_compl)
+                print(f"ERROR response: {err_str}")
             awb_idx += cmd_len
 
-
-class awe_cmd_list:
-    """
-    Reads the commands file and turns them into the enum for sending cmds
-    """
-    def __init__(self, file="../lib_awe/src/include_internal/ProxyIDs.h"):
-        self.enum_dict = {}
-
-        with open(file) as ef:
-            file_content = ef.read()
-            enum_pattern = re.compile(r"typedef\s+enum\s*{([^}]*)}\s*(\w+);", re.DOTALL)
-            matches = enum_pattern.search(file_content)
-            if matches:
-                enum_body = matches.group(1)
-                # Remove all comments
-                enum_body = re.sub(r'//.*?\n|/\*.*?\*/', '', enum_body, flags=re.DOTALL)
-                enum_entries = enum_body.split(',')
-                current_value = -1
-                for entry in enum_entries:
-                    entry = entry.strip()
-                    if entry:
-                        if '=' in entry:
-                            name, value = entry.split('=')
-                            name = name.strip()
-                            value = int(value.strip())
-                            current_value = value
-                        else:
-                            name = entry.strip()
-                            current_value += 1
-                        self.enum_dict[name] = current_value
-
-    def lookup(self, cmd):
-        return self.enum_dict[cmd]
-
-    def get_keys(self):
-        return self.enum_dict
-
-class awe_error_codes:
-    """
-    Reads the error file and turns them into a string from the err code
-    """
-    def __init__(self, file="../lib_awe/src/include_internal/Errors.h"):
-        self.file = file
-        self.define_dict = self._parse_defines()
-
-    def _parse_defines(self):
-        define_dict = {}
-        define_pattern = re.compile(r"#define\s+(\w+)\s+\(([-\d]+)\)")
-        
-        with open(self.file, 'r') as ef:
-            file_content = ef.read()
-            for match in define_pattern.findall(file_content):
-                name, value = match
-                define_dict[int(value)] = name
-        
-        return define_dict
-
-    def lookup(self, err_code):
-        return self.define_dict.get(err_code, None)
-
-    def get_keys(self):
-        return self.define_dict
 
 def run_xe(bin_dir, cmds, max_cycles=1000000):
     cmd = f"xsim --max-cycles {max_cycles} --args {bin_dir} {cmds}"
