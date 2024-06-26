80363666c5c9545104d9083dfed4a66a8b6b9173
Ross Owen <ross@xmos.com>
2024-06-26 13:43:59 +0100

    Updates for xccm 1.2 (allows removal of custom cmake file)

--- lib_awe/lib_build_info.cmake
+++ lib_awe/lib_build_info.cmake
@@ -1,12 +1,8 @@
 set(LIB_NAME lib_awe)
 set(LIB_VERSION 0.1.0)
 
-set(LIB_ARCH xs3a)
-set(LIB_INCLUDES api)
-
+set(LIB_INCLUDES api src src/include_internal)
 set(LIB_OPTIONAL_HEADERS awe_conf.h)
-
-set(LIB_ADD_SRC_DIRS src)
-set(LIB_ADD_INC_DIRS inc src src/include_internal)
+set(LIB_COMPILER_FLAGS -O3 -g)
 
 XMOS_REGISTER_STATIC_LIB()
