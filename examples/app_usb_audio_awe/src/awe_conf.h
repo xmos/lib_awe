128ca7bd6928297ed9af3599beb236a3feee5d56
Ross Owen <ross@xmos.com>
2024-06-28 13:19:48 +0100

    - Fix for no audio output in I2S build of app_usb_aud_awe - Add —-pid option to awe_test_utils (default still 0x18)

--- examples/app_usb_audio_awe/src/awe_conf.h
+++ examples/app_usb_audio_awe/src/awe_conf.h
@@ -6,8 +6,14 @@
 #include "xua.h"
 
 #define AWE_DSP_THREAD_NUM           (3)
+
+#ifdef I2S_ONLY
+#define AWE_INPUT_CHANNELS           (I2S_CHANS_ADC)
+#define AWE_OUTPUT_CHANNELS          (I2S_CHANS_DAC)
+#else
 #define AWE_INPUT_CHANNELS           (NUM_USB_CHAN_OUT)
 #define AWE_OUTPUT_CHANNELS          (NUM_USB_CHAN_IN)
+#endif
 
 #define AWE_HEAP_SIZE                (44*1024)
 
