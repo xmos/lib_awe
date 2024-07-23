5c418b7f7fee1e52074bf19b736e09371623cd76
Ed <ed@xmos.com>
2024-07-23 09:24:15 +0100

    Use lib flags for source also

--- lib_awe/lib_build_info.cmake
+++ lib_awe/lib_build_info.cmake
@@ -3,7 +3,7 @@ set(LIB_VERSION 0.1.0)
 
 set(LIB_INCLUDES api src src/include_internal)
 set(LIB_OPTIONAL_HEADERS awe_conf.h)
-set(LIB_COMPILER_FLAGS -O3 -g)
+set(LIB_COMPILER_FLAGS -O3 -g -mcmodel=large -mno-dual-issue)
 set(LIB_DEPENDENT_MODULES   "lib_xud(2.3.1)"
                             "lib_logging(3.2.0)"
                             "lib_xassert(4.2.0)"
