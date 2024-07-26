2d77e97b8cdfe1d050073fc43bfbf4bdfc065def
Ed <ed@xmos.com>
2024-07-26 11:30:09 +0100

    Fix exception by locking of channels in mulit-tuning host cases

--- lib_awe/src/awe_tuning_usb_hid.c
+++ lib_awe/src/awe_tuning_usb_hid.c
@@ -23,13 +23,14 @@ void awe_usb_hid(chanend_t c_hid_to_host, chanend_t c_hid_from_host) {
     /* Mark the OUT endpoint to be ready to receive data from host */
     XUD_SetReady_Out(ep_hid_from_host, (unsigned char*)g_hid_from_host_buffer);
 
-    int ready_for_next_response_packet = 0; // This guards the select which receives responses from AWE
+    int host_requests_hid_data = 0;         // This guards the select which receives responses from AWE. It holds off packets on long responses to match HID poll rate
+    int transaction_underway = 0;           // Flasg to stat that a transaction (cmd, response) is underway
     XUD_Result_t result;                    // Because we share the channel end from two different threads
     unsigned length;                        // potentially we only want to select on it if not already busy
 
     /* Wait for response from XUD and service relevant EP */
     SELECT_RES(
-        CASE_GUARD_THEN(g_xAWETuningInstance.c_tuning_to_host, ready_for_next_response_packet, input_from_awe),
+        CASE_GUARD_THEN(g_xAWETuningInstance.c_tuning_to_host, transaction_underway && host_requests_hid_data, input_from_awe),
         CASE_THEN(c_hid_from_host, xud_hid_output),
         CASE_THEN(c_hid_to_host, xud_hid_input)
         )
@@ -45,11 +46,12 @@ void awe_usb_hid(chanend_t c_hid_to_host, chanend_t c_hid_from_host) {
             g_hid_to_host_buffer[i+1] = chanend_in_word(g_xAWETuningInstance.c_tuning_to_host);
         }
         int response_finished = chanend_in_word(g_xAWETuningInstance.c_tuning_to_host);
-        ready_for_next_response_packet = 0; // Clear before synch
+        host_requests_hid_data = 0; // Clear before synch
         chanend_out_end_token(g_xAWETuningInstance.c_tuning_to_host);
         chanend_check_end_token(g_xAWETuningInstance.c_tuning_to_host);
         // If we have transmitted the whole response then unlock the tuning channels
         if(response_finished){
+            transaction_underway = 0;
             lock_release(g_xAWETuningInstance.l_api_lock);
         }
 
@@ -66,7 +68,8 @@ void awe_usb_hid(chanend_t c_hid_to_host, chanend_t c_hid_from_host) {
             // If the first packet of a message then lock the tuning channels
             if(sequence == 0){
                 lock_acquire(g_xAWETuningInstance.l_api_lock);
-                ready_for_next_response_packet = 1; // enable reception of response
+                host_requests_hid_data = 1; // enable reception of response
+                transaction_underway = 1;
             }
             chanend_out_word(g_xAWETuningInstance.c_tuning_from_host, num_words);
             for(int i = 0; i < num_words; i++) {
@@ -78,9 +81,9 @@ void awe_usb_hid(chanend_t c_hid_to_host, chanend_t c_hid_from_host) {
         XUD_SetReady_Out(ep_hid_from_host, (unsigned char*)g_hid_from_host_buffer);
 
         continue;
-    xud_hid_input:  /* HID IN to host. This indicates the previous XUD_SetReady_In is complete */
+    xud_hid_input:  /* HID IN to host. This indicates the host is ready for data */
         XUD_SetData_Select(c_hid_to_host, ep_hid_to_host, &result);
-        ready_for_next_response_packet = 1;
+        host_requests_hid_data = 1;
 
         continue;
 
