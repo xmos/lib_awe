7d0e4292c7cbf045f703e14ce9554a86a48d0485
Ed <ed@xmos.com>
2024-08-05 17:35:25 +0100

    Split tests

--- tests/test_hw_stream.py
+++ tests/test_hw_stream.py
@@ -16,14 +16,10 @@ from hardware_test_tools.check_analyzer_output import check_analyzer_output
 
 @pytest.mark.hw
 @pytest.mark.parametrize("awb_load_method", ["HID", "FFS"])
-@pytest.mark.parametrize("audio_transport", ["UA", "I2S"])
-def test_stream_out(pytestconfig, awb_load_method, audio_transport, flash_ua_with_ffs):
+def test_stream_out(pytestconfig, awb_load_method, flash_ua_with_ffs):
     adapter_dut, adapter_harness = get_xtag_ids(pytestconfig)
 
-    if audio_transport == "I2S" and awb_load_method == "FFS":
-        pytest.skip() # FFS/I2S not a supported build config yet
-
-    config = audio_transport + "" if awb_load_method == "HID" else "_FFS"
+    config = "UA" + ("" if awb_load_method == "HID" else "_FFS")
 
     with AweDut(adapter_dut, config) as dut:
         awe = awe_hid_comms()
@@ -48,10 +44,7 @@ def test_stream_out(pytestconfig, awb_load_method, audio_transport, flash_ua_wit
         with AudioAnalyzerHarness(
             adapter_harness, analyzer_dir, attach="xscope"
         ) as harness:
-            if audio_transport == "UA":
-                xsig_config = Path(__file__).parent / "output_sine_2ch.json"
-            else:
-                xsig_config = Path(__file__).parent / "input_sine_2ch.json"
+            xsig_config = Path(__file__).parent / "output_sine_2ch.json"
             assert xsig_config.exists()
 
             with XsigOutput(fs, None, xsig_config, dut.dev_name) as xsig_proc:
@@ -68,4 +61,61 @@ def test_stream_out(pytestconfig, awb_load_method, audio_transport, flash_ua_wit
                 + f"\n\nxscope output:\n{xscope_lines}\n"
                 + f"xsig output:\n{xsig_proc.proc_output}"
             )
+            assert len(failures) == 0, f"Failures: {fail_str}"
+
+
+@pytest.mark.hw
+@pytest.mark.parametrize("awb_load_method", ["HID", "FFS"])
+def test_stream_i2s_loop(pytestconfig, awb_load_method):
+    adapter_dut, adapter_harness = get_xtag_ids(pytestconfig)
+
+    if awb_load_method == "FFS":
+        pytest.skip() # FFS/I2S not a supported build config yet
+
+    config = "I2S" + ("" if awb_load_method == "HID" else "_FFS")
+
+    with AweDut(adapter_dut, config) as dut:
+        awe = awe_hid_comms()
+
+        if awb_load_method == "FFS":
+            awe.load_awb_from_ffs("playBasic_3thread.awb")
+        else:
+            awb = (
+                Path(__file__).parents[1]
+                / "examples"
+                / "audioweaver"
+                / "awb_files"
+                / "playBasic_3thread.awb"
+            )
+            assert awb.exists()
+            awe.send_awb(awb)
+
+        fs = 48000
+        duration = 30
+
+        analyzer_dir = Path(__file__).parents[2] / "sw_audio_analyzer"
+        with AudioAnalyzerHarness(
+            adapter_harness, analyzer_dir, attach="xscope"
+        ) as harness:
+
+            xsig_config = Path(__file__).parent / "input_sine_2ch.json"
+            assert xsig_config.exists()
+
+            with XsigOutput(fs, None, xsig_config, dut.dev_name) as xsig_proc:
+                time.sleep(duration)
+                harness.terminate()
+                xscope_lines = harness.proc_stdout + harness.proc_stderr
+
+            with open(xsig_config) as file:
+                xsig_json = json.load(file)
+
+            failures = check_analyzer_output(xscope_lines, xsig_json["out"])
+            fail_str = (
+                "\n".join(failures)
+                + f"\n\nxscope output:\n{xscope_lines}\n"
+                + f"xsig output:\n{xsig_proc.proc_output}"
+            )
+
+            print(xscope_lines, file=sys.stderr)
+
             assert len(failures) == 0, f"Failures: {fail_str}"
